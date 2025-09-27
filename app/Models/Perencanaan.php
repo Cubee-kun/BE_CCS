b82af0d5002ff436e63c587b5abc5da828336b53
Ghaida zalfa <ghaidagabang17@gmail.com>
2025-09-28 05:26:40 +0700

    s

--- app/Models/Perencanaan.php
+++ app/Models/Perencanaan.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-// use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @OA\Schema(
@@ -36,7 +36,7 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
  */
 class Perencanaan extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'user_id',
